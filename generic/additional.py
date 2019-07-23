e21764f305ffc3ad037f22f519708f0dee4a78d8
JediKnightChan <jediknightchannel@yandex.ru>
2019-07-23 21:44:30 +0900

    Traceback in log

--- generic/additional.py
+++ generic/additional.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.conf import settings
+import traceback
 
 
 def api_safe_run(logger, token_required=False):
@@ -16,12 +17,20 @@ def api_safe_run(logger, token_required=False):
                 logger.error("{}: ZeroDivisionError. Body request is {}".format(func.__name__, request.body))
                 return JsonResponse({'error': 'ZeroDivisionError', 'success': False}, status=400)
             except KeyError as e:
-                logger.error("{}: {} error ({}). Body request is {}".format(func.__name__,
-                                                                            type(e), e, request.body))
+                logger.error("{}: {} error ({}). Body request is {}.\b Traceback: {}".format(func.__name__,
+                                                                                             type(e),
+                                                                                             e,
+                                                                                             request.body,
+                                                                                             traceback.format_exc()
+                                                                                             ))
                 return JsonResponse({'error': 'KeyError', 'success': False}, status=400)
             except Exception as e:
-                logger.error("{}: Unknown {} error ({}). Body request is {}".format(func.__name__,
-                                                                                    type(e), e, request.body))
+                logger.error("{}: Unknown {} error ({}). Body request is {}. Traceback: {}".format(func.__name__,
+                                                                                                   type(e),
+                                                                                                   e,
+                                                                                                   request.body,
+                                                                                                   traceback.format_exc()
+                                                                                                   ))
                 return JsonResponse({'error': 'Unknown error', 'success': False}, status=500)
         return func_wrapper
     return decorator
